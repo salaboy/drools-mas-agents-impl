b4e0fb9d5bcfa51c6a42b0195772010e9caf931a
salaboy <salaboy@gmail.com>
2012-01-11 15:59:57 -0300

    - initial commit

---
+++ new-action-agent-client/src/main/java/com/salaboy/jbpm5/dev/guide/newactionagentclient/App.java
@@ -0,0 +1,13 @@
+package com.salaboy.jbpm5.dev.guide.newactionagentclient;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
